38c8e96a7672ab1d219aeb93f33e90bfd45cb2ac
Aziz M. Bookwala <me@azizmb.in>
2013-10-22 21:56:50 +0400

    Setup fci for python mode

--- init-python-mode.el
+++ init-python-mode.el
@@ -3,5 +3,14 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+;;----------------------------------------------------------------------------
+;; Python - fill column indicator
+;;----------------------------------------------------------------------------
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (set-fill-column 79)))
+
+(add-hook 'python-mode-hook 'fci-mode)
 
 (provide 'init-python-mode)
