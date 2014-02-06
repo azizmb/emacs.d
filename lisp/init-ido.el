0e7f217187c00a7e62b49e12672929962b3505fe
Aziz M. Bookwala <aziz.mansur@gmail.com>
2014-02-06 11:14:15 +0400

    Adding ido function to open files that require root permission with sudo

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -29,6 +29,12 @@
 
  (global-set-key [(meta f11)] 'sanityinc/ido-choose-from-recentf))
 
+;; Open file with sudo if required
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 
 (provide 'init-ido)
