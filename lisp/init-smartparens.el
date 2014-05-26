bfc5ebe547564927d836e68debb06b5ccc083699
Aziz M. Bookwala <aziz.mansur@gmail.com>
2014-05-26 10:51:31 +0400

    Fix cua region

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -14,4 +14,19 @@
 
 (add-hook 'minibuffer-setup-hook 'conditionally-enable-smartparens-mode)
 
+
+;; ToDo: Remove when this is fixed,
+;; https://github.com/Fuco1/smartparens/issues/271
+(unless (fboundp 'cua-replace-region)
+  (defun cua-replace-region ()
+    "Replace the active region with the character you type."
+    (interactive)
+    (let ((not-empty (and cua-delete-selection (cua-delete-region))))
+      (unless (eq this-original-command this-command)
+        (let ((overwrite-mode
+               (and overwrite-mode
+                    not-empty
+                    (not (eq this-original-command 'self-insert-command)))))
+          (cua--fallback))))))
+
 (provide 'init-smartparens)
