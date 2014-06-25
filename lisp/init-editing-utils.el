2d97a5c686352471760b7ea8db26864ea11d3f3b
Aziz M. Bookwala <aziz.mansur@gmail.com>
2014-06-25 10:01:26 +0400

    Adding toggle quotes util

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -363,5 +363,33 @@ With arg N, insert N newlines."
 (guide-key-mode 1)
 (diminish 'guide-key-mode)
 
+;;----------------------------------------------------------------------------
+;; Toggle quotes
+;; http://stackoverflow.com/questions/15580913/is-there-a-way-to-toggle-a-string-between-single-and-double-quotes-in-emacs
+;;----------------------------------------------------------------------------
+(defun toggle-quotes ()
+  "Toggle single quoted string to double or vice versa, and
+  flip the internal quotes as well.  Best to run on the first
+  character of the string."
+  (interactive)
+  (save-excursion
+    (re-search-backward "[\"']")
+    (let* ((start (point))
+           (old-c (char-after start))
+           new-c)
+      (setq new-c
+            (case old-c
+              (?\" "'")
+              (?\' "\"")))
+      (setq old-c (char-to-string old-c))
+      (delete-char 1)
+      (insert new-c)
+      (re-search-forward old-c)
+      (backward-char 1)
+      (let ((end (point)))
+        (delete-char 1)
+        (insert new-c)
+        (replace-string new-c old-c nil (1+ start) end)))))
+
 
 (provide 'init-editing-utils)
