7f877dbad1c05561fcfa7217b09f248edcb12e4e
Aziz M. Bookwala <me@azizmb.in>
2015-09-14 22:32:36 +0400

    Disabling persp

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -3,15 +3,16 @@
 (projectile-global-mode)
 
 (define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
-(define-key projectile-mode-map [?\s-/] 'projectile-persp-switch-project)
+;; (define-key projectile-mode-map [?\s-/] 'projectile-persp-switch-project)
+(define-key projectile-mode-map [?\s-/] 'projectile-switch-project)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
 (define-key projectile-mode-map [?\s-g] 'projectile-ag)
 
 (setq projectile-switch-project-action 'projectile-vc)
 
-(require-package 'perspective)
-(require-package 'persp-projectile)
-(persp-mode)
+;; (require-package 'perspective)
+;; (require-package 'persp-projectile)
+;; (persp-mode)
 
 
 (provide 'init-projectile)
