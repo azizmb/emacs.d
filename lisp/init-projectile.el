a8c8ccb8ce0c4d5d935e1f4144c57743e3d7ef86
Aziz M. Bookwala <me@azizmb.in>
2015-09-14 11:10:23 +0400

    Switch from projectile-grep to projectile-ag
    Blazing fast search

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -5,7 +5,7 @@
 (define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
 (define-key projectile-mode-map [?\s-/] 'projectile-persp-switch-project)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
-(define-key projectile-mode-map [?\s-g] 'projectile-grep)
+(define-key projectile-mode-map [?\s-g] 'projectile-ag)
 
 (setq projectile-switch-project-action 'projectile-vc)
 
