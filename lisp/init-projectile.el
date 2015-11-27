6cc07c21784f50073e8eb56e21f96bdbe8b6e6ba
Aziz M. Bookwala <me@azizmb.in>
2015-11-27 20:56:04 +0400

    Get rid of persp once and for all

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -8,17 +8,12 @@
 (helm-projectile-on)
 
 (define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
-;; (define-key projectile-mode-map [?\s-/] 'projectile-persp-switch-project)
 (define-key projectile-mode-map [?\s-/] 'projectile-switch-project)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
 (define-key projectile-mode-map [?\s-g] 'projectile-ag)
 
 (setq projectile-switch-project-action 'helm-projectile)
 ;; (setq projectile-switch-project-action 'projectile-vc)
-
-;; (require-package 'perspective)
-;; (require-package 'persp-projectile)
-;; (persp-mode)
 
 
 (provide 'init-projectile)
