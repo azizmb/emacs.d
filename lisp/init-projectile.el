baa09bd386ecf4041b871ac648a7cd466ff27814
Aziz M. Bookwala <me@azizmb.in>
2016-02-11 17:30:39 +0400

    Switching to helm-ag from projectile-ag

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -10,7 +10,7 @@
 (define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
 (define-key projectile-mode-map [?\s-/] 'projectile-switch-project)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
-(define-key projectile-mode-map [?\s-g] 'projectile-ag)
+(define-key projectile-mode-map [?\s-g] 'helm-projectile-ag)
 
 (setq projectile-switch-project-action 'helm-projectile)
 ;; (setq projectile-switch-project-action 'projectile-vc)
